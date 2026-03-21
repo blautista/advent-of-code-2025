34ffd9d9e9871b3f328bc6966cec731e1e3a8703
blautista <bautistaromanazzi@gmail.com>
2026-03-21 01:35:55 -0300

    add 05 part 2

--- days/05/index.ts
+++ days/05/index.ts
@@ -3,9 +3,56 @@ import { advent } from "../utils/utils.ts";
 import { lines } from "../utils/strings.ts";
 import { sumOf, toIntArray } from "../utils/array.ts";
 
+type RuleMap = Map<number, ReadonlySet<number>>;
+
+function isCorrectlyOrdered(rules: RuleMap, update: number[]): boolean {
+  for (const [i, n] of update.entries()) {
+    const rule = rules.get(n);
+
+    if (rule) {
+      const previous = new Set(update.slice(0, i));
+      const intersection = rule.intersection(previous);
+
+      if (intersection.size > 0) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+function orderUpdate(rules: RuleMap, update: number[]): number[] {
+  const ordered = [update[0]];
+
+  for (const n of update.slice(1)) {
+    let added = false;
+    const rule = rules.get(n)!;
+
+    if (rule) {
+      for (const [i, previous] of ordered.entries()) {
+        if (rule.has(previous)) {
+          ordered.splice(i, 0, n);
+          added = true;
+          break;
+        }
+      }
+    }
+
+    if (!added) {
+      ordered.push(n);
+    }
+  }
+
+  return ordered;
+}
+
+function getUpdateValue(update: number[]): number {
+  return update[Math.floor(update.length / 2)];
+}
+
 advent({
   day: 5,
-  // test: true,
 
   parse(raw) {
     const [rules, updates] = raw.split(EOL + EOL);
@@ -26,22 +73,19 @@ advent({
 
   one: ({ rules, updates }) =>
     sumOf(updates, (update) => {
-      for (const [i, n] of update.entries()) {
-        const rule = rules.get(n);
-
-        if (rule) {
-          const previous = new Set(update.slice(0, i));
-          const intersection = rule.intersection(previous);
-
-          if (intersection.size > 0) {
-            return 0;
-          }
-        }
+      if (!isCorrectlyOrdered(rules, update)) {
+        return 0;
       }
-      return update[Math.floor(update.length / 2)];
+
+      return getUpdateValue(update);
     }),
 
-  two() {
-    return 0;
-  },
+  two: ({ rules, updates }) =>
+    sumOf(updates, (update) => {
+      if (isCorrectlyOrdered(rules, update)) {
+        return 0;
+      }
+
+      return getUpdateValue(orderUpdate(rules, update));
+    }),
 });
